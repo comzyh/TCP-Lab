f98f9c5742b36341d45cf1484ffd7b39d7d82a4f
Comzyh <comzyh@gmail.com>
2020-07-05 19:33:06 +0800

    WrappingInt32

--- libsponge/wrapping_integers.cc
+++ libsponge/wrapping_integers.cc
@@ -1,5 +1,7 @@
 #include "wrapping_integers.hh"
 
+#include <iostream>
+
 // Dummy implementation of a 32-bit wrapping integer
 
 // For Lab 2, please replace with a real implementation that passes the
@@ -13,10 +15,7 @@ using namespace std;
 //! Transform an "absolute" 64-bit sequence number (zero-indexed) into a WrappingInt32
 //! \param n The input absolute 64-bit sequence number
 //! \param isn The initial sequence number
-WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
-    DUMMY_CODE(n, isn);
-    return WrappingInt32{0};
-}
+WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) { return WrappingInt32{uint32_t(n + isn.raw_value())}; }
 
 //! Transform a WrappingInt32 into an "absolute" 64-bit sequence number (zero-indexed)
 //! \param n The relative sequence number
@@ -29,6 +28,11 @@ WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
 //! and the other stream runs from the remote TCPSender to the local TCPReceiver and
 //! has a different ISN.
 uint64_t unwrap(WrappingInt32 n, WrappingInt32 isn, uint64_t checkpoint) {
-    DUMMY_CODE(n, isn, checkpoint);
-    return {};
+    WrappingInt32 wrap_checkpoint = wrap(checkpoint, isn);
+    uint32_t diff = n - wrap_checkpoint;
+    // This case should never be persent. Unforunately, it's include in test cases.
+    if (diff & 0x80000000 && diff + checkpoint <= UINT32_MAX) {
+        return checkpoint + diff;
+    }
+    return checkpoint + static_cast<int32_t>(diff);
 }
