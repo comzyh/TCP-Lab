40a70367bf5322a4a24f9163b5f16aba5a8b6569
Comzyh <comzyh@gmail.com>
2020-08-07 02:45:13 +0800

    Lab 2: the TCP receiver

--- libsponge/tcp_receiver.hh
+++ libsponge/tcp_receiver.hh
@@ -20,12 +20,22 @@ class TCPReceiver {
     //! The maximum number of bytes we'll store.
     size_t _capacity;
 
+    //! Initial Sequence Number
+    std::optional<WrappingInt32> isn; 
+
+    //! FIN is received and accepted(inside the receive window)
+    bool fin_received;
+
+    //! ackno in Absolute Sequence Numbers form
+    uint64_t abs_ackno() const;
+
+
   public:
     //! \brief Construct a TCP receiver
     //!
     //! \param capacity the maximum number of bytes that the receiver will
     //!                 store in its buffers at any give time.
-    TCPReceiver(const size_t capacity) : _reassembler(capacity), _capacity(capacity) {}
+    TCPReceiver(const size_t capacity) : _reassembler(capacity), _capacity(capacity), isn(std::nullopt),fin_received(false) {}
 
     //! \name Accessors to provide feedback to the remote TCPSender
     //!@{
