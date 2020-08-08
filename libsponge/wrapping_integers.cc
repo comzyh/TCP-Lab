835c97bf332fa181e4f21197a4add0bda625fafc
Comzyh <comzyh@gmail.com>
2020-08-08 18:05:07 +0800

    Add some comments to unwrap

--- libsponge/wrapping_integers.cc
+++ libsponge/wrapping_integers.cc
@@ -31,8 +31,13 @@ uint64_t unwrap(WrappingInt32 n, WrappingInt32 isn, uint64_t checkpoint) {
     WrappingInt32 wrap_checkpoint = wrap(checkpoint, isn);
     uint32_t diff = n - wrap_checkpoint;
     // This case should never be persent. Unforunately, it's include in test cases.
+    // This case occurs when diff < 0 and checkpoint < -diff, 
+    // which means we are just starting the TCP connection, but the absolute ackno has exceed 2^31,
+    // which means we have over 2GB data on flying, and TCP 32bit acno is not enough.
+    // considering this case we can ONLY deal with the first 1 or 2 packet but not always.
     if (diff & 0x80000000 && diff + checkpoint <= UINT32_MAX) {
         return checkpoint + diff;
     }
+    // this can deal with both diff > 0 and diff < 0 scenarios.
     return checkpoint + static_cast<int32_t>(diff);
 }
